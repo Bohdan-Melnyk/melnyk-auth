7a961012444c7c867127f4ae7ef78e600915effc
BohdanMelnyk <bmelnu@softserveinc.com>
2023-11-13 12:34:08 +0200

    first commit

---
+++ src/main/java/com/blog/melnykauth/MelnykAuthApplication.java
@@ -0,0 +1,13 @@
+package com.blog.melnykauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelnykAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelnykAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/melnykauth/MelnykAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.melnykauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelnykAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
